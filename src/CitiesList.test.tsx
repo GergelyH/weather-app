b3ed59ad1e1fbd77d387bdc7e4401ef7548f0ae6
Gergely Halacsy <gergely.halacsy@gmail.com>
2023-07-23 21:47:34 +0200

    Fix mocking wrong library, add BrowserRouter to make tests pass

--- src/CitiesList.test.tsx
+++ src/CitiesList.test.tsx
@@ -1,33 +1,33 @@
-import { useNavigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { render, screen, fireEvent } from '@testing-library/react';
 import CitiesList from './CitiesList';
 
-const mockUseNavigate = jest.fn();
-jest.mock("react-router-dom", () => ({
-    ...jest.requireActual("react-router-dom"),
-    useNavigate: () => mockUseNavigate,
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router", () => ({
+  ...(jest.requireActual("react-router") as any),
+  useNavigate: () => mockedUsedNavigate
 }));
 
 it('renders the input cities', () => {
     const cities = ['Vienna', 'Chicago'];
-    render(<CitiesList cities={cities}/>);
+    render(
+        <BrowserRouter>
+            <CitiesList cities={cities}/>
+        </BrowserRouter>
+    );
     for(let city of cities){
         const cityElement = screen.getByText(new RegExp(`${city}`, 'i'));
         expect(cityElement).toBeInTheDocument();
     }
 }) 
 
-
-it('call navigates to /add-city on button press', () => {
-//     const mockHistoryPush = jest.fn();
-//   useNavigate.mockReturnValue({
-//     push: mockHistoryPush,
-//   });
-    const {getByRole} = render(<CitiesList cities={[]}/>);
-    const buttonElement = getByRole('button');
-    fireEvent.click(buttonElement);
-    expect(mockUseNavigate).toHaveBeenCalledWith("/", {
-        replace: true,
-    });
-
+it('call navigates to /add-city on button press', async () => {
+    const {getByRole} = render(
+        <BrowserRouter>
+            <CitiesList cities={['Asd']}/>
+        </BrowserRouter>
+    );
+    const buttonElement = screen.getByRole('button', {name: '+'});
+    await fireEvent.click(buttonElement);
+    expect(mockedUsedNavigate).toHaveBeenCalledWith('/add-city');
 })
