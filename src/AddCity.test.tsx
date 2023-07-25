215a8a25613b3f5d7d7befa8a570f1f3f2b9fe8a
Gergely Halacsy <gergely.halacsy@gmail.com>
2023-07-26 00:14:27 +0200

    Add test for AddCity that searches

--- src/AddCity.test.tsx
+++ src/AddCity.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import AddCity from "./AddCity";
 
@@ -8,8 +8,10 @@ test('Renders a input field', () => {
     expect(textField).toBeInTheDocument();
 })
 
-test('', () => {
-    const { getByRole } = render(<AddCity />);
-    const textField = getByRole('textbox');
-    expect(textField).toBeInTheDocument();
+test('can search for a city and it appears', () => {
+    render(<AddCity />);
+    const textField = screen.getByRole('textbox');
+    fireEvent.change(textField, {target: {value: 'ienna'}})
+    const cityElement = screen.getByText(/Vienna/);
+    expect(cityElement).toBeInTheDocument();
 })
