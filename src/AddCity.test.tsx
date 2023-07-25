c428d707f0bf564439fbeaa8b1a51cb27502b158
Gergely Halacsy <gergely.halacsy@gmail.com>
2023-07-26 01:42:26 +0200

    Instead of checking just the render, check a search result

--- src/AddCity.test.tsx
+++ src/AddCity.test.tsx
@@ -2,16 +2,21 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 import AddCity from "./AddCity";
 
-test('Renders a input field', () => {
-    const { getByRole } = render(<AddCity />);
-    const textField = getByRole('textbox');
-    expect(textField).toBeInTheDocument();
+test('shows the correct result list after search', () => {
+    render(<AddCity />);
+    const textField = screen.getByRole('textbox');
+    fireEvent.change(textField, {target: {value: 'ku'}})
+    const capitalsToCheck = ['Kuwait City', 'Kuala Lumpur', "Nukuʻalofa", 'Baku'];
+    for(let capital of capitalsToCheck){
+        const cityElement = screen.getByText(capital);
+        expect(cityElement).toBeInTheDocument();
+    }
 })
 
 test('can search for a city and it appears', () => {
     render(<AddCity />);
     const textField = screen.getByRole('textbox');
     fireEvent.change(textField, {target: {value: 'ienna'}})
-    const cityElement = screen.getByText(/Vienna/);
+    const cityElement = screen.getByText('Vienna');
     expect(cityElement).toBeInTheDocument();
 })
