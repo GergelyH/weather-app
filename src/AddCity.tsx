c103f8d46207dd574b7e0d0378af334cf903eacd
Gergely Halacsy <gergely.halacsy@gmail.com>
2023-07-26 21:26:06 +0200

    Add selection feature and render save button if a city is selected

--- src/AddCity.tsx
+++ src/AddCity.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import classNames from "classnames";
+
 import './Spinner.css';
 import { selectableCities } from './selectableCities';
 
@@ -6,8 +8,18 @@ function AddCity() {
     const [isLoading, setIsLoading] = useState(false);
     const [searchValue, setSearchValue] = useState('');
     const [searchResults, setSearchResults] = useState<string[]>([]);
+    const [selectedCityIndex, setSelectedCityIndex] = useState<number | null>(null);
     
     useEffect(calculateSearchResults,[searchValue]);
+
+    function onSearchResultClick(cityIndex: number) {
+        if (cityIndex !== selectedCityIndex) {
+            setSelectedCityIndex(cityIndex);
+        }
+        else {
+            setSelectedCityIndex(null);
+        }
+    }
 
     function onInputChange(e: React.FormEvent<HTMLInputElement>){
         setIsLoading(true);
@@ -16,20 +28,36 @@ function AddCity() {
 
     function calculateSearchResults(){
         const res:string[] = [];
-        for(let city of selectableCities){
-            if(city.toLocaleLowerCase().includes(searchValue.toLowerCase())){
-                res.push(city);
+        if (searchValue) {
+            for (let city of selectableCities) {
+                if (city.toLocaleLowerCase().includes(searchValue.toLowerCase())) {
+                    res.push(city);
+                }
             }
         }
         setSearchResults(res);
         setIsLoading(false);
     }
     
+    const cityList = searchResults.map((city, index) =>
+        <p 
+            className={classNames({ 'selected': index === selectedCityIndex })}
+            onClick={() => onSearchResultClick(index)}
+            key={index}
+        >
+            {city}
+        </p>
+    );
+    const loadedContent = <div>
+        {cityList}
+        {selectedCityIndex !== null && <button data-testid='save-button'></button>}
+    </div>
+
     return <>
         <input onChange={onInputChange} data-testid='city-search-textbox'></input>
         {isLoading
          ? <div className="spinner"></div>
-         : searchResults.map((city, index) => <p key={index}>{city}</p>)}
+            : loadedContent} 
     </>
 }
 
