87bfb84b5032f686547580a02df279a0b2bae6a9
GergelyH <gergely.halacsy@gmail.com>
2023-07-27 08:42:14 +0200

    style - add empty lines to separate logical blocks

--- src/AddCity.test.tsx
+++ src/AddCity.test.tsx
@@ -4,16 +4,20 @@ import AddCity from "./AddCity";
 
 test('can search for a city and it appears', () => {
     render(<AddCity />);
+
     const textField = screen.getByRole('textbox');
     fireEvent.change(textField, {target: {value: 'ienna'}})
+
     const cityElement = screen.getByText('Vienna');
     expect(cityElement).toBeInTheDocument();
 })
 
 test('shows the correct result list after search', () => {
     render(<AddCity />);
+
     const textField = screen.getByRole('textbox');
     fireEvent.change(textField, {target: {value: 'ku'}})
+
     const capitalsToCheck = ['Kuwait City', 'Kuala Lumpur', "Nukuʻalofa", 'Baku'];
     for(let capital of capitalsToCheck){
         const cityElement = screen.getByText(capital);
@@ -24,12 +28,15 @@ test('shows the correct result list after search', () => {
 test('can only save a city after a successful search and city selection', () => {
     render(<AddCity />);
     expect(screen.queryByTestId('save-button')).toBeNull();
+
     const textField = screen.getByRole('textbox');
     fireEvent.change(textField, {target: {value: 'ndo'}})
     expect(screen.queryByTestId('save-button')).toBeNull();
+
     const cityElement = screen.getByText('London');
     fireEvent.click(cityElement);
     expect(cityElement).toHaveClass('selected');
+
     const saveButton = screen.getByTestId('save-button');
     fireEvent.click(saveButton);
 })
